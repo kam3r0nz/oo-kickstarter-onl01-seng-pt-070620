c41d45c85fb529743f08cec4affdb367b976be4c
Kameron Hungerford <kam.hungerford@gmail.com>
2020-08-05 01:40:17 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+  attr_accessor :title
+  attr_reader :backers
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(name)
+    @backers << name
+  end
+end
